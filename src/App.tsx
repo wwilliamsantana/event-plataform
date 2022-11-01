73d0826f68967b178d0a636a60d00a2ea81f44d6
William Silva <euwilliamsantana@gmail.com>
2022-11-01 18:33:23 -0300

    Create Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-import { gql, useQuery } from "@apollo/client"
-
-
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
-interface Lessson {
-  id: string
-  title: string
-}
+import { Event } from "./pages/Event";
 
 export function App() {
-
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(res => {
-  //     console.log(res.data)
-  //   })
-  // }, [])
-
-  const { data } = useQuery<{ lessons: Lessson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map(item => {
-        return <li>{item.title}</li>
-      })}
-    </ul>
+      <Event />
   )
 }
 
