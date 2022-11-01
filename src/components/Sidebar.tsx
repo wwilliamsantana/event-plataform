5be7e0848c347f46a97ebb8b804f348fedd6a536
William Silva <euwilliamsantana@gmail.com>
2022-11-01 19:47:36 -0300

    API - Consumindo dados

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,31 @@
 
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+    id
+    lessonType
+    availableAt
+    title
+    slug
+  }
+}
+`
 
+interface GetLessonsQueryResponse{
+  lessons: {
+    id: string
+    title: string
+    availableAt: string
+    slug: string
+    lessonType: "live" | "class"
+  }[]
+}
 
 export function Sidebar() {
+  const {data} = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
 
   return (
    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
@@ -11,12 +33,19 @@ export function Sidebar() {
         Cronograma de aulas
       </span>
     <div className="flex flex-col gap-8">
+
+      {data?.lessons.map(lesson => {
+        return (
           <Lesson
-            title="Aula 01"
-            type="live"
-            availableAt={new Date()}
-            slug="aula-1"
+            key={lesson.id}
+            title={lesson.title}
+            type={lesson.lessonType}
+            availableAt={new Date(lesson.availableAt)}
+            slug={lesson.slug}
           />
+        )
+      })}
+      
     </div>
 
    </aside>
