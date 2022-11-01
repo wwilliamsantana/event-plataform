92d012ad1c5d3f444e3eaee3973fd3b58c99bae7
William Silva <euwilliamsantana@gmail.com>
2022-11-01 19:45:11 -0300

    Estrutura - Header + Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,24 @@
+
+import { Lesson } from "./Lesson";
+
+
+
 export function Sidebar() {
+
   return (
-    <h1>Sidebar</h1>
+   <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl text-white pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
+    <div className="flex flex-col gap-8">
+          <Lesson
+            title="Aula 01"
+            type="live"
+            availableAt={new Date()}
+            slug="aula-1"
+          />
+    </div>
+
+   </aside>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,33 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.tsx", "./index.html"],
+  content: ['./src/**/*.tsx', './index.html'],
+
   theme: {
-    colors: {
-      green: {
-        300: '#00B37E',
-        500: '#00875F',
-        700: '#015F43',
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
       },
-      blue: {
-        500: '#81D8F7',
+      colors: {
+        green: {
+          300: '#00B37E',
+          500: '#00875F',
+          700: '#015F43',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '#FBA94C',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A',
+        },
       },
-      orange: {
-        500: '#FBA94C',
-      },
-      red: {
-        500: '#F75A68',
-      },
-      gray: {
-        100: '#E1E1E6',
-        200: '#C4C4CC',
-        300: '#8D8D99',
-        500: '#323238',
-        600: '#29292E',
-        700: '#121214',
-        900: '#09090A'
-      }
     },
-    extend: {},
   },
+
   plugins: [],
 }
