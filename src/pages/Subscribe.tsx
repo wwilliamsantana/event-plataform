19d699df4edff6e07e553452a27af45512c70053
William <euwilliamsantana@gmail.com>
2022-11-05 15:49:47 -0300

    Button Att

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -31,10 +31,8 @@ export function Subscribe(){
   return (
     <div className="bg-backgroundSub bg-no-repeat bg-top ">
       <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-
         
           <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-
             <div className="max-w-[640px]">
               <Logo/>
               <h1 className="mt-8 text-[2.5rem] leading-tight">Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong> </h1>
@@ -65,8 +63,8 @@ export function Subscribe(){
                 disabled={loading}
                 className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50 flex items-center justify-center"
                 >
-                  {loading ? <LoadingAnimation props={"w-4 h-4 text-white"}/> : " "}
-                  <span>Garantir minha vaga</span>
+                  {loading ? <LoadingAnimation props={"w-5 h-5 text-white"}/> : <span>Garantir minha vaga</span>}
+                  
                 </button>
               </form>
 
